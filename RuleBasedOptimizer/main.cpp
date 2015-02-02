5bfd5c2b2ff2976bb19e8d3d719e5aaada876c4f
Christian Deckert <christian.deckert@zentrale2.com>
2015-02-02 11:17:42 +0100

    Initial Commit

---
+++ RuleBasedOptimizer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RuleBasedOptimizer
+//
+//  Created by Christian Deckert on 02/02/15.
+//  Copyright (c) 2015 Christian Deckert. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
