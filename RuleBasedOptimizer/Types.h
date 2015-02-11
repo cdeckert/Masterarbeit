f3438a03ebb6330847ab24900e7d12e26fea468f
Christian Deckert <christian.deckert@zentrale2.com>
2015-02-11 15:33:55 +0100

    main / types

---
+++ RuleBasedOptimizer/Types.h
@@ -0,0 +1,9 @@
+//
+//  Types.h
+//
+
+#ifndef RuleBasedOptimizer_Types_h
+#define RuleBasedOptimizer_Types_h
+
+
+#endif
--- RuleBasedOptimizer/main.cpp
+++ RuleBasedOptimizer/main.cpp
@@ -6,13 +6,20 @@
 
 #include <iostream>
 #include "TreeGenerator.h"
+#include "ExhaustiveTransformation.h"
+#include "Rule.h"
 
 
 int main(int argc, const char * argv[]) {
     std::cout << "Start!\n";
     //Reservoir<Plan> * planReservoir = new Reservoir<Plan>(30);
     //planReservoir->get_new_entry();
-    TreeGenerator::generate();
+     EquivalenceClass<Bitvector> * equivalence = TreeGenerator::generate();
+    
+    
+    ExhaustiveTransformation<EquivalenceClass<Bitvector>, PlanNode<Bitvector>, Rule<EquivalenceClass<Bitvector>, PlanNode<Bitvector>>> * transformation = new ExhaustiveTransformation<EquivalenceClass<Bitvector>, PlanNode<Bitvector>, Rule<EquivalenceClass<Bitvector>, PlanNode<Bitvector>>>::ExhaustiveTransformation(new Rule<EquivalenceClass<Bitvector>, PlanNode<Bitvector>>());
+    
+    transformation->execute(equivalence);
     
     return 0;
 }
