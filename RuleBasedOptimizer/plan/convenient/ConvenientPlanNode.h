4b44131cb09e6e908f74413afa75a0089fc9dca7
Christian Deckert <christian.deckert@zentrale2.com>
2015-03-24 14:31:40 +0100

    readme

---
+++ RuleBasedOptimizer/plan/convenient/ConvenientPlanNode.h
@@ -0,0 +1,13 @@
+//
+//  ConvenientPlanNode.h
+//  RuleBasedOptimizer
+//
+//  Created by Christian Deckert on 24/03/15.
+//  Copyright (c) 2015 Christian Deckert. All rights reserved.
+//
+
+#ifndef RuleBasedOptimizer_ConvenientPlanNode_h
+#define RuleBasedOptimizer_ConvenientPlanNode_h
+
+
+#endif
