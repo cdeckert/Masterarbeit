7e2282bb77cce619cd586e5599a6162eaa0913d7
Christian Deckert <christian.deckert@zentrale2.com>
2015-06-07 16:14:35 +0200

    old cost Estimator

---
+++ RuleBasedOptimizer/cost/CostEstimator.h
@@ -0,0 +1,19 @@
+//
+//  CostEstimator.h
+//
+
+#ifndef RuleBasedOptimizer_CostEstimator_h
+#define RuleBasedOptimizer_CostEstimator_h
+
+
+template <typename PlanNode_t>
+class CostEstimator
+{
+	typedef typename PlanNode_t::EquivalenceClass_t EquivalenceClass_t;
+	
+public:
+    virtual void findCheapestPlan(EquivalenceClass_t &) = 0;
+};
+
+
+#endif
